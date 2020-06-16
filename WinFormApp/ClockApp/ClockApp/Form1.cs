23a4da455b1143c23b7dd1fbfe8a3d4969db4dbb
Moon-SSung <xnwoa89@gmail.com>
2020-06-16 14:11:19 +0900

    20200616 Winform Study

--- WinFormApp/ClockApp/ClockApp/Form1.cs
+++ WinFormApp/ClockApp/ClockApp/Form1.cs
@@ -22,5 +22,27 @@ namespace ClockApp
             DateTime now = DateTime.Now;
             label1.Text = now.ToString("HH:mm:ss");
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Form 로드시 발생");
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(MessageBox.Show("진짜 닫을래?", "경고", 
+                MessageBoxButtons.YesNo,MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                e.Cancel = false;   // e는 EventArgs 이다. 모든 이벤트에 있다.
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
